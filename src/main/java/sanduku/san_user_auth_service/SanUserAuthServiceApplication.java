b574f64bd6a2f2bb50d7dd1eeb81440f9cb428d5
ugolb <eugen.nagirnij@gmail.com>
2018-07-11 21:11:48 +0200

    First commit

---
+++ src/main/java/sanduku/san_user_auth_service/SanUserAuthServiceApplication.java
@@ -0,0 +1,12 @@
+package sanduku.san_user_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanUserAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanUserAuthServiceApplication.class, args);
+	}
+}
